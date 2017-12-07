6c23d0eba15edf387813979f58adf929204bfdfb
Florian Tönjes <skatepainter@yahoo.de>
2017-12-07 23:19:53 +0100

    Changed from assigned users to user contacts

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,6 +3,8 @@ import {Todo} from '../shared/todo.model';
 import {TodoService} from '../shared/todo.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {TodoForm} from '../shared/todo-form.model';
+import {User} from '../shared/user.model';
+import {UserService} from '../shared/user.service';
 
 @Component({
   selector: 'app-todo',
@@ -11,8 +13,9 @@ import {TodoForm} from '../shared/todo-form.model';
 export class TodoComponent implements OnInit {
   todoListId: Number;
   todo: Todo = new Todo();
+  contacts: User[];
 
-  constructor(private todoService: TodoService, private route: ActivatedRoute, private router: Router) {
+  constructor(private todoService: TodoService, private userService: UserService, private route: ActivatedRoute, private router: Router) {
     this.todo.todoForm = new TodoForm();
   }
 
@@ -22,6 +25,10 @@ export class TodoComponent implements OnInit {
       this.todoService.getTodo(+params['todoId']).then(response => {
         this.todo = response;
       });
+    });
+
+    this.userService.getContacts().then(response => {
+      this.contacts = response;
     });
   }
 
