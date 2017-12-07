ae64f49f50dfa79d7fd5f478f884defac985c03a
Florian Tönjes <skatepainter@yahoo.de>
2017-12-07 11:34:53 +0100

    Initializing Todo to an empty instance now.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,15 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {Todo} from '../shared/todo.model';
 import {TodoService} from '../shared/todo-service.service';
 import {ActivatedRoute} from '@angular/router';
+import {TodoForm} from '../shared/todo-form.model';
 
 @Component({
   selector: 'app-todo',
   templateUrl: 'todo.component.html'
 })
 export class TodoComponent implements OnInit {
-  todo: Todo;
+  todo: Todo = new Todo();
 
   constructor(private todoService: TodoService, private route: ActivatedRoute) {
+    this.todo.todoForm = new TodoForm();
   }
 
 
