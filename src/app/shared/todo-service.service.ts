8ad7bc5752b6e0a1f9cabc2aa655fe29eb2e7803
Florian Tönjes <skatepainter@yahoo.de>
2017-12-07 12:51:42 +0100

    Added IDs to assigned todos

--- src/app/shared/todo-service.service.ts
+++ src/app/shared/todo-service.service.ts
@@ -66,6 +66,13 @@ export class TodoService {
     })
       .toPromise()
       .then(response => {
+        const todos: Todo[] = response['_embedded']['todos'] as Todo[];
+
+        todos.forEach(todo => {
+          const selfLink: String = todo['_links']['self']['href'];
+          todo.id = Number(selfLink.substr(selfLink.length - 1, 1));
+        });
+
         return response['_embedded']['todos'] as Todo[];
       });
   }
