219ae30cfa66dee40e044a782cf47e186778b3d4
Florian Tönjes <skatepainter@yahoo.de>
2017-12-07 11:28:01 +0100

    Added getTodo method to TodoService

--- src/app/shared/todo-service.service.ts
+++ src/app/shared/todo-service.service.ts
@@ -29,8 +29,8 @@ export class TodoService {
       });
   }
 
-  getTodos(todoId: Number): Promise<Todo[]> {
-    return this.http.get(`http://localhost:8080/api/v1/todoLists/${todoId}/todos?projection=todoProjection`, {
+  getTodos(todoListId: Number): Promise<Todo[]> {
+    return this.http.get(`http://localhost:8080/api/v1/todoLists/${todoListId}/todos?projection=todoProjection`, {
       headers: new HttpHeaders().set('Authorization', 'Basic dXNlcjpwYXNzd29yZA==')
         .set('Content-Type', 'application/x-www-form-urlencoded')
     })
@@ -44,6 +44,17 @@ export class TodoService {
         });
         console.log(todos);
         return todos;
+      });
+  }
+
+  getTodo(todoId: Number): Promise<Todo> {
+    return this.http.get(`http://localhost:8080/api/v1/todos/${todoId}?projection=todoProjection`, {
+      headers: new HttpHeaders().set('Authorization', 'Basic dXNlcjpwYXNzd29yZA==')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+    })
+      .toPromise()
+      .then(response => {
+        return response as Todo;
       });
   }
 
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Todo} from '../shared/todo.model';
+import {TodoService} from '../shared/todo-service.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-todo',
   templateUrl: 'todo.component.html'
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todo: Todo;
+
+  constructor(private todoService: TodoService, private route: ActivatedRoute) {
+  }
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.todoService.getTodo(+params['todoId']).then(response => {
+        this.todo = response;
+        console.log(response);
+      });
+    });
+  }
 }
