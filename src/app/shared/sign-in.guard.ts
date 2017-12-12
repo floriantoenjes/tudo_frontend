ee863b413d5c9493a0d691df3135714e2f68c78a
Florian Tönjes <skatepainter@yahoo.de>
2017-12-12 10:38:18 +0100

    Added redirection to sign in page when not signed in.

--- src/app/shared/sign-in.guard.ts
+++ src/app/shared/sign-in.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import {AuthService} from './auth.service';
 
@@ -7,13 +7,17 @@ import {AuthService} from './auth.service';
 export class SignInGuard implements CanActivate {
 
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-      return this.authService.isLoggedIn();
+      if (this.authService.isLoggedIn()) {
+        return true;
+      } else {
+        this.router.navigateByUrl('/sign_in');
+      }
   }
 }
