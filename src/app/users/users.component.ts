7f50e75b71e6e11993428a3bc90af80c449f15c0
Florian Tönjes <skatepainter@yahoo.de>
2017-12-12 11:01:13 +0100

    Using AuthService in users component now.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../shared/user.service';
 import {User} from '../shared/user.model';
+import {AuthService} from '../shared/auth.service';
 
 @Component({
   selector: 'app-users',
@@ -10,14 +11,14 @@ export class UsersComponent implements OnInit {
   title: String = 'User List';
   users: User[];
 
-  constructor(private userService: UserService) {
+  constructor(private authService: AuthService, private userService: UserService) {
   }
 
 
   ngOnInit(): void {
     this.userService.getUsers().then(response => {
-      // ToDo: Change id to be the one of the signed in user
-      this.users = response.filter(user => user.id !== 1);
+      // ToDo: Change to triple equals
+      this.users = response.filter(user => user.id != this.authService.getUserId());
     });
   }
 }
