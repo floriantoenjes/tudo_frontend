6f8616bede0fcc8d90018cd3af7415b8abf19137
Florian Tönjes <skatepainter@yahoo.de>
2017-12-19 21:41:58 +0100

    Completed a todo.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,8 +16,7 @@ export class UsersComponent implements OnInit {
 
   ngOnInit(): void {
     this.userService.getUsers().then(response => {
-      // ToDo: Change to triple equals
-      this.users = response.filter(user => user.id != this.authService.getUserId());
+      this.users = response.filter(user => user.id !== Number(this.authService.getUserId()));
     });
   }
 }
